7e6fa512cda8b007a40e7d8a8861ea9003d11bae
imat-cww <imat.cww@gmail.com>
2024-10-11 17:21:15 +0900

    Updates

--- static/js/8731.222b3fe9.chunk.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunklbol_logs_dev=self.webpackChunklbol_logs_dev||[]).push([[8731],{8731:s=>{s.exports=JSON.parse('[{"GUID":"zosit.lbol.test.utsuho","Name":"UtsuhoCharacter","Version":"1.4.8","Url":"https://thunderstore.io/c/touhou-lost-branch-of-legend/p/Zosit/UtsuhoCharacter/"}]')}}]);
